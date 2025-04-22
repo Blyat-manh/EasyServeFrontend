78bfd7f26282f2f9d0bcfbff5d2b088de68c446a
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2025-04-22 10:13:24 +0200

    Add files via upload

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
   const history = useNavigate();
+  const [userRole, setUserRole] = useState('');
+
+  useEffect(() => {
+    const role = localStorage.getItem('userRole');
+    setUserRole(role);
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
+    localStorage.removeItem('userRole');
     history('/');
   };
 
   const navigateToEmployeeManagement = () => {
     history('/employee-management');
   };
+
   const navigateToInventoryManagement = () => {
     history('/inventory-management');
   };
+
   const navigateToPlaceOrder = () => {
     history('/place-order');
   };
@@ -23,8 +32,15 @@ const Dashboard = () => {
     <div>
       <h1>Dashboard</h1>
       <button onClick={handleLogout}>Logout</button>
-      <button onClick={navigateToEmployeeManagement}>Manage Employees</button>
-      <button onClick={navigateToInventoryManagement}>Manage Inventory</button>
+
+      {/* Mostrar botones solo si el usuario es Encargado */}
+      {userRole === 'encargado' && (
+        <>
+          <button onClick={navigateToEmployeeManagement}>Manage Employees</button>
+          <button onClick={navigateToInventoryManagement}>Manage Inventory</button>
+        </>
+      )}
+
       <button onClick={navigateToPlaceOrder}>Place Order</button>
     </div>
   );
