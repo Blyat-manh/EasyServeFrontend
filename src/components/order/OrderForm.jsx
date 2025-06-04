a3763c7038249582bbba25ac413bf90e4d341031
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2025-06-04 09:14:43 +0200

    Update OrderForm.jsx

--- src/components/order/OrderForm.jsx
+++ src/components/order/OrderForm.jsx
@@ -1,6 +1,5 @@
 import '../../styles/orderForm.scss';
 
-
 const OrderForm = ({ inventory, onAddToOrder, selectedTable, setSelectedTable, availableTables }) => {
   return (
     <div className='oreder-form'>
@@ -8,35 +7,43 @@ const OrderForm = ({ inventory, onAddToOrder, selectedTable, setSelectedTable, a
       <select
         id="table-select"
         value={selectedTable}
-        onChange={(e) => setSelectedTable(e.target.value)}
+        onChange={(e) => setSelectedTable(Number(e.target.value))} // <-- Cambiado: asegura tipo number
       >
         <option value="">Selecciona una mesa</option>
-        {availableTables.map((table) => (
-          <option key={table.id} value={table.id}>
-            Mesa {table.table_number}
-          </option>
-        ))}
+        {availableTables
+          .filter((table) => table.status !== "reserved") // <-- Solo mesas no reservadas
+          .map((table) => (
+            <option key={table.id} value={table.id}>
+              Mesa {table.table_number}
+            </option>
+          ))}
       </select>
 
-        <h2>Menu: </h2>
+      <h2>Menu: </h2>
       <h3>Bebidas</h3>
       <ul>
-        {inventory.filter(i => i.type === "bebida").sort((a, b) => a.price - b.price).map(item => (
-          <li key={item.id}>
-            {item.name} - ${item.price}
-            <button onClick={() => onAddToOrder(item)}>Agregar</button>
-          </li>
-        ))}
+        {inventory
+          .filter(i => i.type === "bebida")
+          .sort((a, b) => a.price - b.price)
+          .map(item => (
+            <li key={item.id}>
+              {item.name} - ${item.price}
+              <button onClick={() => onAddToOrder(item)}>Agregar</button>
+            </li>
+          ))}
       </ul>
 
       <h3>Tapas</h3>
       <ul>
-        {inventory.filter(i => i.type === "tapa").sort((a, b) => a.price - b.price).map(item => (
-          <li key={item.id}>
-            {item.name} - ${item.price}
-            <button onClick={() => onAddToOrder(item)}>Agregar</button>
-          </li>
-        ))}
+        {inventory
+          .filter(i => i.type === "tapa")
+          .sort((a, b) => a.price - b.price)
+          .map(item => (
+            <li key={item.id}>
+              {item.name} - ${item.price}
+              <button onClick={() => onAddToOrder(item)}>Agregar</button>
+            </li>
+          ))}
       </ul>
     </div>
   );
